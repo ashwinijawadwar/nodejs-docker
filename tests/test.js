3420cb299ed3f86c44a29a21d7cf7d8b36413643
ashwinijawadwar <jawadwarashwini2004@gmail.com>
2025-01-26 15:36:49 +0530

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return status 200', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+  });
+});
